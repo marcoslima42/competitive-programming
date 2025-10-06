2ed9d74a62efb23d2125ac6459b3ac8674840f47
Marcos Lima <araujolima728@gmail.com>
2025-10-05 21:36:07 -0300

    Update: wheel

---
+++ learningCpp/stl/wheel.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#define REP(i, a, b)    for(int i = a; i < b; i++)
+#define _ ios_base::sync_with_stdio(0); cin.tie(0);
+#define endl '\n'
+
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fl;
+
+using namespace std;
+
+vector<int> p;
+
+int main(){ _
+    int n, x, value, count=0, ans=0;
+
+    cin>>n>>x;
+
+    REP(i, 0, n){
+        cin>>value;
+        p.push_back(value);
+    }
+
+    REP(i, 0, n){   
+        if(count!=2){
+            
+        
+        }else{
+
+            
+            count=0;
+        }
+    }    
+    
+    return 0;
+}
