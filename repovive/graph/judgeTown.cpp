db31a2a43b991b307f662ad22791d7ddd740f2af
marcoslima42 <araujolima728@gmail.com>
2026-05-03 01:48:34 -0300

    add: basic graph problems

---
+++ repovive/graph/judgeTown.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findJudge(int n, vector<vector<int>>& trust) {
+        vector<int> in(n+1), out(n+1);
+        int judge = -1;
+
+        for(int i = 0; i < trust.size(); i++){
+            out[trust[i][0]]++;
+            in[trust[i][1]]++;
+        }
+        for(int i = 1; i < in.size(); i++){
+            if((in[i] == n-1) && (out[i] == 0))
+                return i;
+        }
+        return judge;
+    }
+};
---
+++ repovive/graph/roadConstruction.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    int n, m;
+    int u, v;
+    
+    cin >> n >> m;
+    vector<bool> center(n, true);
+
+
+    for(int i = 0; i < m; i++){
+        cin >> u >> v;
+        center[u-1] = false;
+        center[v-1] = false;
+    }
+
+    cout << n-1 << "\n";
+    int centerCity = 0;
+
+    for(int i = 0; i < n; i++){
+        if(center[i] == true){
+            centerCity = i+1;
+            break;
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        if(i+1 != centerCity)
+            cout << centerCity << " " << i+1 << "\n";
+    }
+    
+    return 0;
+}
