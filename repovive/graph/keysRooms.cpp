742e2db40ec2f3bf993fba58fdc728d5c51a6355
marcoslima42 <araujolima728@gmail.com>
2026-05-10 12:11:18 -0300

    add: graph problem

---
+++ repovive/graph/keysRooms.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/keys-and-rooms/
+
+class Solution {
+public:
+    void dfs(int root, vector<vector<int>> &rooms, vector<bool> &visited){
+        visited[root] = true;
+
+        for(auto v : rooms[root]){
+            if(!visited[v])
+                dfs(v, rooms, visited);
+        }
+    }
+   
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> visited(rooms.size(), false);
+
+        dfs(0, rooms, visited);
+
+        return find(visited.begin(), visited.end(), false) == visited.end() ? true : false;
+    }
+};
