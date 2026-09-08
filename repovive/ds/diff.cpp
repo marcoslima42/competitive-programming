b375d54aa6224abd45c031035215e43126fa15e9
marcoslima42 <araujolima728@gmail.com>
2026-09-08 13:33:26 -0300

    add: diff code

---
+++ repovive/ds/diff.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    
+    //mudar indice 1 ao indice 3 indice 
+    int start = 1; int end = 3; int v = 3;
+    vector<int> diff = {0, 0, 0, 0, 0};
+    int size = diff.size();
+
+    //setar no inicio do intervalo
+    diff[1] += v;
+    //setar no fim do intervalo 
+    if(end + 1 < size)
+        diff[end + 1] -= v;
+
+    for(int i = 1; i < size; i++){  
+        diff[i] += diff[i - 1];
+    }  
+
+    for(auto x : diff){
+        cout << x << " ";
+    }
+    return 0;
+}
